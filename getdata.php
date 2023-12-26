50cc656e8b7fe364ceeae077112a23a566bae7dd
Viktor T. Toth <vttoth@vttoth.com>
2023-12-26 02:35:04 -0500

    Read actual sampling frequency from header file

--- getdata.php
+++ getdata.php
@@ -5,6 +5,23 @@ $column = $_GET['column'];
 //$file='80030009n.csv';
 //$column='NBPd [mmHg]';
 
+$hea = fopen("./HEAD/" . str_replace("n.csv", ".hea", $file), 'r');
+$freq = 3.6e6;
+while ($line = fgets($hea))
+{
+  if (preg_match("/^[^#]/", $line))
+  {
+    $fields = explode(" ", $line);
+    $field2 = explode("/", $fields[2]);
+    $freq = 3600.0 * $field2[1];
+    break;
+  } 
+}
+fclose($hea);
+
+//echo "Factor is " . $freq . "\n";
+//die;
+
 $csv = fopen("./DATA/" . $file, 'r');
 $headers = fgetcsv($csv);
 
@@ -18,7 +35,7 @@ echo "[";
 while ($row = fgetcsv($csv)) {
     if (isset($row[$index]) && $row[$index] != "") {
       if ($count++ > 0) echo ",";
-      echo '{"x": ' . ($row[0] / 3.6e6);
+      echo '{"x": ' . ($row[0] / $freq);
       echo ',"y": ' . $row[$index] . "}";
     }
 }
